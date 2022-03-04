b0c1db005d36d9bfcb5a8de578e97c857599c1b0
elicodes89 <elicodes89@gmail.com>
2022-03-04 19:15:01 +0000

    tested if guests list started at 0. failed before passing

--- codeclan_caraoke/src/room.py
+++ codeclan_caraoke/src/room.py
@@ -3,8 +3,11 @@ class Room:
     def __init__(self, name):
         self.name = name
         self.songs_queue = []
-        self.guests_queue = []
+        self.guests_list = []
 
     def get_songs_queue(self):
         
         return len(self.songs_queue)
+
+    def get_guests_list(self):
+        return len(self.guests_list)
--- codeclan_caraoke/tests/room_test.py
+++ codeclan_caraoke/tests/room_test.py
@@ -6,5 +6,9 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room = Room("Rock Room")
 
+
     def test_songs_queue_starts_at_0(self):
         self.assertEqual(0, self.room.get_songs_queue())
+    
+    def test_guests_list_starts_at_0(self):
+        self.assertEqual(0, self.room.get_guests_list())
